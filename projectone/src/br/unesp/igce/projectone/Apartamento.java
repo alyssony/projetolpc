977e406fa6fefa451767b3789f3565d7dbe377d6
Guilherme Moneda Rodrigues <guimoneda@gmail.com>
2013-03-21 15:53:33 -0300

    Classe Ap

---
+++ projectone/src/br/unesp/igce/projectone/Apartamento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.projectone;
+
+/**
+ *
+ * @author aluno
+ */
+public class Apartamento {
+    private String numero;
+    private String contas;
+  
+     Apartamento (String numero){
+         this.numero = numero;
+         this.contas = "";
+     }
+ }
+  
